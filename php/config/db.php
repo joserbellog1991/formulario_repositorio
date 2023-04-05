785a6bde89a296cbbfe0adaeedc438100ccd2949
Jose Bello <joserbellog1991@gmail.com>
2023-04-05 10:22:14 -0400

    se actualiza config/db

--- php/config/db.php
+++ php/config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');
-define('DB_USER', 'usaservi_user');
-define('DB_PASS', 'soportec2019-');
-define('DB_NAME', 'usaservi_votacion');
+define('DB_HOST', 'localhost');//host del la base de datos
+define('DB_USER', 'usuario');//usuario de la base de datos
+define('DB_PASS', 'contraseña');//contraseña de la base de datos
+define('DB_NAME', 'nombre de base de datos');//nombre de la base de datos
 ?>
